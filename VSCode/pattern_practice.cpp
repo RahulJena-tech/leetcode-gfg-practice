2fc0d6e1e91ae742d82c0e7b5b52effbf3a200b9
Rahul Jena <rahuljenawork@gmail.com>
2026-08-31 02:57:29 +0530

    Add pattern 9

--- VSCode/pattern_practice.cpp
+++ VSCode/pattern_practice.cpp
@@ -78,8 +78,32 @@ void pattern8(int n){
         cout<<endl;
     }
 }
+
+void pattern9(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i; j++){
+            cout<<' ';
+        }
+        for(int j=0; j<2*i+1; j++){
+            cout<<'*';
+        }
+        cout<<endl;
+    }
+    for(int i=n; i>0; i--){
+        cout<<" ";
+        for(int j=0; j<n-i; j++){
+            cout<<" ";
+        }
+        for(int j=0; j<2*i-1; j++){
+            cout<<'*';
+        }
+        cout<<endl;
+    }
+}
+
+
 int main(){
     int x = 4;
-    pattern8(x);
+    pattern9(x);
     return 0;
 }
