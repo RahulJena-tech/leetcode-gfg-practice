c7015a2b909fb5b25f1bbd76da62a615aee7be17
Rahul Jena <rahuljenawork@gmail.com>
2026-09-09 19:19:55 +0530

    Add pattern 19

--- VSCode/pattern_practice.cpp
+++ VSCode/pattern_practice.cpp
@@ -216,8 +216,35 @@ void pattern18(int n){
     }
 }
 
+void pattern19(int n){
+    for(int i=0; i<n; i++){
+        for(int j=n-i; j>0; j--){
+            cout<<"* ";
+        }
+        for(int k=0; k<(2*i); k++){
+            cout<<"  ";
+        }
+        for(int l=n-i; l>0; l--){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int m=0; m<n; m++){
+        for(int o=0; o<=m; o++){
+            cout<<"* ";
+        }
+        for(int p=2*(n-m-1); p>0; p--){
+            cout<<"  ";
+        }
+        for(int q=0; q<=m; q++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
     int x = 5;
-    pattern18(x);
+    pattern19(x);
     return 0;
 }
